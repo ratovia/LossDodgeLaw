56c5fe82667bf5c99e22110c2d9700c8fb82db63
ratovia <ratovia@hotmail.co.jp>
2020-06-29 03:20:47 +0900

    LossDodgeLaw

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,65 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-app>
+    <v-card
+      class="align-center justify-center pa-4 mx-auto my-5"
+      max-width="550"
+      min-height="76"
+      outlined
+    >
+      <div class="font-weight-bold">
+        あなたはこのゲームに参加しますか？
+      </div>
+      <ul>
+        <li>コイントス、表が出たら1万円払わなければならない</li>
+        <li>コイントス、裏が出たら1万円もらえる</li>
+      </ul>
+    </v-card>
+    <div class="d-flex mx-auto">
+      <div class="my-2 mr-5">
+        <v-btn @click="cointoss" x-large width="200px" color="success" dark>参加する</v-btn>
+      </div>
+      <div class="my-2">
+        <v-btn x-large width="200px" color="error" dark>参加しない</v-btn>
+      </div>
+    </div>
+    <div class="mx-auto my-2" v-show="flag">
+      コイントスの結果 
+      <span class="font-weight-bold red--text text--lighten-1"> {{ this.sides }} </span>
+      でした
+    </div>
+    <h3
+      class="mx-auto"
+    >現在の残金は {{ price }} 円です</h3>
+    <!-- <v-content>
+      <LossDodgeLaw/>
+    </v-content> -->
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import LossDodgeLaw from './components/LossDodgeLaw';
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+  // components: {
+  //   LossDodgeLaw,
+  // },
+
+  data: () => ({
+    price: 0,
+    goal: 100000,
+    flag: false,
+    sides: "表"
+  }),
+  methods: {
+    cointoss: function () {
+      let side = Math.floor(Math.random() * Math.floor(2))
+
+      this.price += side ? 10000 : -10000
+      this.flag = true
+      this.sides = side ? "表" : "裏" 
+    }
+  }
+};
+</script>
---
+++ src/components/LossDodgeLaw.vue
@@ -0,0 +1,46 @@
+<template>
+  <!-- <v-question-section 
+    class=" mx-auto"
+    width="100vw"
+  >
+    <h1>あなたはこのゲームに参加しますか？</h1>
+    <ul>
+      <li>表が出たら一万円払わなければならない</li>
+      <li>裏が出たら二万円もらえる</li>
+    </ul>
+    <div class="buttons d-flex">
+      <button>参加する</button>
+      <button>参加しない</button>
+    </div>
+    <h3>あなたの残金 {{ price }} 円</h3>
+  </div> -->
+  <v-card
+    class="align-center justify-center pa-4 mx-auto my-5"
+    max-width="550"
+    min-height="76"
+    outlined
+  >
+    <div :class="`text-${model}`">
+      あなたはこのゲームに参加しますか？
+    </div>
+    <ul>
+      <li>表が出たら一万円払わなければならない</li>
+      <li>裏が出たら二万円もらえる</li>
+    </ul>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data: function (){
+    return {
+      price: 0,
+      goal: 100000
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
